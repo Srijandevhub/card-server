b4377200047ee2b59d317022675650e419afc225
Your Name <s.r.j.s.n2017@gmail.com>
2024-07-27 09:29:14 +0530

    rc

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { authenticateToken } = require('./middlewares/auth');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3001", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
